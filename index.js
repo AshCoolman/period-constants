c02fe7d30136248344279ca33f4a75261599de8d
Ashley Coolman <writetofish@gmail.com>
2016-01-15 14:01:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const MS = 1
+const SECOND =      SEC =   1000 * MS
+const MINUTE =      MIN =   60 * SEC
+const HOUR =        HR =    60 * MIN
+const DAY =                 24 * HOUR
+const WEEK =        WK =    7 * DAY
+const YEAR =        YR =    365 * DAY
+const DECADE =      DEC =   10 * YEAR
+const CENTUARY =    CEN =   100 * YEAR
+const MILLENIUM =           1000 * YEAR
+
+module.exports = {
+    MS,
+    SECOND,         SEC,
+    MINUTE,         MIN,
+    HOUR,           HR,
+    DAY,
+    WEEK,           WK,
+    YEAR,           YR,
+    DECADE,         DEC,
+    CENTUARY,       CEN,
+    MILLENIUM
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Intro
+
+Simply periods that are commonly used in javascript:
+ - 100% constants - no API to learn
+ - Singular grammer (DAY not DAYS)
+ - Shorthand versions when commonly exists
+
+```
+const MS = 1
+const SECOND =      SEC =   1000 * MS
+const MINUTE =      MIN =   60 * SEC
+const HOUR =        HR =    60 * MIN
+const DAY =                 24 * HOUR
+const WEEK =        WK =    7 * DAY
+const YEAR =        YR =    365 * HOUR
+const DECADE =      DEC =   10 * YEAR
+const CENTUARY =    CEN =   100 * YEAR
+const MILLENIUM =           100 * YEAR
+```
