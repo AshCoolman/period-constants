cfef68564ababb9d7d37756e6c233a773dca4121
Ashley Coolman <writetofish@gmail.com>
2016-01-20 08:40:55 +0000

    feat (es5): default file is es5 compatible;

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const MS = 1
-const SECOND =      SEC =   1000 * MS
-const MINUTE =      MIN =   60 * SEC
-const HOUR =        HR =    60 * MIN
-const DAY =                 24 * HOUR
-const WEEK =        WK =    7 * DAY
-const YEAR =        YR =    365 * DAY
-const DECADE =      DEC =   10 * YEAR
-const CENTUARY =    CEN =   100 * YEAR
-const MILLENIUM =           1000 * YEAR
+"use strict";
+
+var MS = 1;
+var SECOND = SEC = 1000 * MS;
+var MINUTE = MIN = 60 * SEC;
+var HOUR = HR = 60 * MIN;
+var DAY = 24 * HOUR;
+var WEEK = WK = 7 * DAY;
+var YEAR = YR = 365 * DAY;
+var DECADE = DEC = 10 * YEAR;
+var CENTUARY = CEN = 100 * YEAR;
+var MILLENIUM = 1000 * YEAR;
 
 module.exports = {
-    MS,
-    SECOND,         SEC,
-    MINUTE,         MIN,
-    HOUR,           HR,
-    DAY,
-    WEEK,           WK,
-    YEAR,           YR,
-    DECADE,         DEC,
-    CENTUARY,       CEN,
-    MILLENIUM
-}
+    MS: MS,
+    SECOND: SECOND, SEC: SEC,
+    MINUTE: MINUTE, MIN: MIN,
+    HOUR: HOUR, HR: HR,
+    DAY: DAY,
+    WEEK: WEEK, WK: WK,
+    YEAR: YEAR, YR: YR,
+    DECADE: DECADE, DEC: DEC,
+    CENTUARY: CENTUARY, CEN: CEN,
+    MILLENIUM: MILLENIUM
+};
